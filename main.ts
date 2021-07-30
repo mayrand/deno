47f82c58644a2f6c382c2b019ef783c8afe1e6bd
oldakan <andrzej.oldak@medicover.pl>
2021-07-30 13:19:31 +0200

    Trying to rewrite node package to deno

--- main.ts
+++ main.ts
@@ -1,25 +1,51 @@
-import { PullParser } from "https://deno.land/x/xmlp/mod.ts";
+import * as path from "https://deno.land/std@0.103.0/path/mod.ts";
+import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+const cli = Deno.args;
+var $ = cheerio.load(Deno.readFileSync(path.resolve(__dirname, cli[0])))
 
-// create a pull parser instance
-const parser = new PullParser();
-
-// create an ES6 generator
-const uint8Array = await Deno.readFile(
-//   ".\\BookmarksDuplicatesRemover\\bookmarks_27.07.2021.html",
-//   ".\\BookmarksDuplicatesRemover\\note.xml",
-  "note.xml",
-);
-const events = parser.parse(uint8Array);
-
-// pull events, using iterator
-const event = events.next();
-if (event.value) {
-    console.log(event.value.name);
+function getCategories($a) {
+    var $node = $a.closest('DL').prev();
+    var title = $node.text();
+    var add_date = $node.attr("add_date");
+    var last_modified = $node.attr("last_modified");
+    if ($node.length > 0 && title.length > 0) {
+        return [{
+            'name': title,
+            'last_modified': typeof last_modified === "undefined" ? null : last_modified ,
+            'add_date': typeof add_date === "undefined" ? null : add_date,
+        }].concat(getCategories($node));
+    } else {
+        return [];
+    }
 }
 
-// using spread operator
-console.log([...events].filter(({ name }) => {
-    return name === 'text';
-}).map(({ text, cdata }) => {
-    return cdata ? `<![CDATA[${text}]]>` : text;
-}));
+var jsonbmArray = []
+$('a').each(function(index, a) {
+    let $a = $(a)
+    let add_date = $a.attr('add_date')
+    let last_modified = $a.attr('last_modified')
+    let description = $a.next('dd').text().split("\n")[0] // ugly but works
+    let categories = getCategories($a)
+    // add level information
+    let new_categories = categories.reverse().map(function(currentValue, index) {
+        return currentValue['level'] = index + 1, currentValue
+    })
+    try {
+        var tags = $a.attr('tags').split(',') || []
+    } catch(e) {
+        var tags = []
+    }
+    let jsonbm = {
+        'description': description,
+        'title': $a.text(),
+        'url': $a.attr('href'),
+        'categories': categories,
+        'tags': tags,
+        'last_modified': typeof last_modified === "undefined" ? null : last_modified ,
+        'add_date': typeof add_date === "undefined" ? null : add_date,
+    }
+    jsonbmArray.push(jsonbm)
+})
+
+Deno.writeFileSync(path.resolve(__dirname, cli[1]), JSON.stringify(jsonbmArray, null, 4))
